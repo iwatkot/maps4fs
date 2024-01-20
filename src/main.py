221619082907d6a2bc9381560f8c18423b445f65
Stan Soldatov <iwatkot@gmail.com>
2024-01-20 04:05:52 +0300

    Added argparser.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
-import src.generate as generate
+import argparse as ap
 
-gm = generate.Map((58.5209, 31.2775), 1024)
+import generate
+
+# TODO: Make it possible to change the map size.
+DISTANCE = 1024
+# 58.5209, 31.2775
+
+parser = ap.ArgumentParser(
+    description="Generate a map for Farming Simulator 22 using OpenStreetMap."
+)
+parser.add_argument("lat", type=float, help="Latitude of the center of the map (e.g. 58.5209)")
+parser.add_argument("lon", type=float, help="Longitude of the center of the map (e.g. 31.2775)")
+
+args = parser.parse_args()
+
+gm = generate.Map((args.lat, args.lon), DISTANCE)
