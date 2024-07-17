d3be291c4691836a23d84034d80283a8564694a0
iwatkot <iwatkot@gmail.com>
2024-07-17 18:01:02 +0200

    Updated ps1 script.

--- run.ps1
+++ run.ps1
@@ -2,7 +2,7 @@ python -m venv .venv
 
 . .\.venv\Scripts\Activate
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 $env:PYTHONPATH = "$(Get-Location)$([IO.Path]::PathSeparator)$env:PYTHONPATH"
 python maps4fs\ui.py
