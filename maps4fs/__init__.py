b3637d81a176152dd4642c77303ad170d5383ccb
Kim Brandwijk <kim.brandwijk@gmail.com>
2025-01-13 15:58:33 -0500

    little fix

--- maps4fs/__init__.py
+++ maps4fs/__init__.py
@@ -3,7 +3,7 @@ from maps4fs.generator.dtm.dtm import DTMProvider
 from maps4fs.generator.dtm.srtm import SRTM30Provider, SRTM30ProviderSettings
 from maps4fs.generator.dtm.usgs import USGSProvider, USGSProviderSettings
 from maps4fs.generator.dtm.nrw import NRWProvider
-from maps4fs.generator.dtm.bavaria import BavariaProvider, BavariaProviderSettings
+from maps4fs.generator.dtm.bavaria import BavariaProvider
 from maps4fs.generator.dtm.niedersachsen import NiedersachsenProvider
 from maps4fs.generator.dtm.hessen import HessenProvider
 from maps4fs.generator.dtm.england import England1MProvider
