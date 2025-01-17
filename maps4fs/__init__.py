fe24859bfd14b1d49f3562ab322d13fd0d6f24b9
Kim Brandwijk <kim.brandwijk@gmail.com>
2025-01-17 14:53:13 -0500

    Norway provider

--- maps4fs/__init__.py
+++ maps4fs/__init__.py
@@ -14,6 +14,7 @@ from maps4fs.generator.dtm.italy import ItalyProvider
 from maps4fs.generator.dtm.flanders import FlandersProvider
 from maps4fs.generator.dtm.spain import SpainProvider
 from maps4fs.generator.dtm.france import FranceProvider
+from maps4fs.generator.dtm.norway import NorwayProvider
 from maps4fs.generator.game import Game
 from maps4fs.generator.map import Map
 from maps4fs.generator.settings import (
