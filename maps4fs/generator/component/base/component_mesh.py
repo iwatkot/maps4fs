f38b67d1669771cd51a5c59bed9b975b856a9083
Stan Soldatov <iwatkot@gmail.com>
2025-01-19 16:30:01 +0100

    Cutting out the center of the mesh.

--- maps4fs/generator/component/base/component_mesh.py
+++ maps4fs/generator/component/base/component_mesh.py
@@ -53,10 +53,12 @@ class MeshComponent(Component):
         image: np.ndarray,
         include_zeros: bool,
         z_scaling_factor: float,
-        resize_factor: float,
+        resize_factor: int,
         apply_decimation: bool,
         decimation_percent: int,
         decimation_agression: int,
+        remove_center: bool,
+        remove_size: int,
     ) -> trimesh.Trimesh:
         """Generates a mesh from the given numpy array.
 
@@ -64,15 +66,20 @@ class MeshComponent(Component):
             image (np.ndarray): The numpy array to generate the mesh from.
             include_zeros (bool): Whether to include zero values in the mesh.
             z_scaling_factor (float): The scaling factor for the Z-axis.
-            resize_factor (float): The resizing factor.
+            resize_factor (int): The resizing factor.
             apply_decimation (bool): Whether to apply decimation to the mesh.
             decimation_percent (int): The percent of the decimation.
             decimation_agression (int): The agression of the decimation.
+            remove_center (bool): Whether to remove the center from the mesh.
+            remove_size (int): The size of the center to remove.
 
         Returns:
             trimesh.Trimesh: The generated mesh.
         """
+        output_x_size, _ = image.shape
         image = image.max() - image
+
+        image = image[::resize_factor, ::resize_factor]
 
         rows, cols = image.shape
         x = np.linspace(0, cols - 1, cols)
@@ -106,15 +113,7 @@ class MeshComponent(Component):
 
         faces_np = np.array(faces)
         mesh = trimesh.Trimesh(vertices=vertices, faces=faces_np)
-
-        # Apply rotation: 180 degrees around Y-axis and Z-axis
-        rotation_matrix_y = trimesh.transformations.rotation_matrix(np.pi, [0, 1, 0])
-        rotation_matrix_z = trimesh.transformations.rotation_matrix(np.pi, [0, 0, 1])
-        mesh.apply_transform(rotation_matrix_y)
-        mesh.apply_transform(rotation_matrix_z)
-
-        # if not include_zeros:
-        mesh.apply_scale([1 / resize_factor, 1 / resize_factor, z_scaling_factor])
+        mesh = MeshComponent.rotate_mesh(mesh)
 
         if apply_decimation:
             percent = decimation_percent / 100
@@ -122,4 +121,112 @@ class MeshComponent(Component):
                 percent=percent, aggression=decimation_agression
             )
 
+        try:
+            if not mesh.is_watertight:
+                mesh = MeshComponent.fix_mesh(mesh)
+        except Exception:
+            pass
+
+        mesh = MeshComponent.mesh_to_output_size(
+            mesh, resize_factor, z_scaling_factor, output_x_size
+        )
+
+        if remove_center:
+            mesh = MeshComponent.remove_center_from_mesh(mesh, remove_size)
+
         return mesh
+
+    @staticmethod
+    def rotate_mesh(mesh: trimesh.Trimesh) -> trimesh.Trimesh:
+        """Rotates the given mesh by 180 degrees around the Y-axis and Z-axis.
+
+        Arguments:
+            mesh (trimesh.Trimesh): The mesh to rotate.
+
+        Returns:
+            trimesh.Trimesh: The rotated mesh.
+        """
+        mesh_copy = mesh.copy()
+
+        rotation_matrices = [
+            trimesh.transformations.rotation_matrix(np.pi, [0, 1, 0]),
+            trimesh.transformations.rotation_matrix(np.pi, [0, 0, 1]),
+        ]
+
+        for rotation_matrix in tqdm(rotation_matrices, desc="Rotating mesh", unit="rotation"):
+            mesh_copy.apply_transform(rotation_matrix)
+
+        return mesh_copy
+
+    @staticmethod
+    def fix_mesh(mesh: trimesh.Trimesh) -> trimesh.Trimesh:
+        """Fixes the given mesh by filling holes, fixing normals, fixing winding, fixing inversion,
+        fixing broken faces, and stitching.
+
+        Arguments:
+            mesh (trimesh.Trimesh): The mesh to fix.
+
+        Returns:
+            trimesh.Trimesh: The fixed mesh.
+        """
+        mesh_copy = mesh.copy()
+
+        fix_methods = [
+            trimesh.repair.fill_holes,
+            trimesh.repair.fix_normals,
+            trimesh.repair.fix_winding,
+            trimesh.repair.fix_inversion,
+            trimesh.repair.broken_faces,
+            trimesh.repair.stitch,
+        ]
+
+        for method in tqdm(fix_methods, desc="Fixing mesh", unit="method"):
+            method(mesh_copy)  # type: ignore
+
+        return mesh_copy
+
+    @staticmethod
+    def mesh_to_output_size(
+        mesh: trimesh.Trimesh, resize_factor: int, z_scaling_factor: float, expected_size: int
+    ) -> trimesh.Trimesh:
+        """Resizes the given mesh to the expected size.
+
+        Arguments:
+            mesh (trimesh.Trimesh): The mesh to resize.
+            resize_factor (int): The resizing factor.
+            z_scaling_factor (float): The scaling factor for the Z-axis.
+            expected_size (int): The expected size.
+
+        Returns:
+            trimesh.Trimesh: The resized mesh.
+        """
+        mesh_copy = mesh.copy()
+
+        mesh_copy.apply_scale([resize_factor / 1, resize_factor / 1, z_scaling_factor])
+
+        x_size, y_size, _ = mesh_copy.extents
+        x_resize_factor = expected_size / x_size
+        y_resize_factor = expected_size / y_size
+
+        mesh_copy.apply_scale([x_resize_factor, y_resize_factor, 1])
+        return mesh_copy
+
+    @staticmethod
+    def remove_center_from_mesh(mesh: trimesh.Trimesh, remove_size: int) -> trimesh.Trimesh:
+        """Removes the center from the given mesh.
+
+        Arguments:
+            mesh (trimesh.Trimesh): The mesh to remove the center from.
+            remove_size (int): The size of the center to remove.
+
+        Returns:
+            trimesh.Trimesh: The mesh with the center removed.
+        """
+        mesh_copy = mesh.copy()
+
+        _, _, z_size = mesh_copy.extents
+
+        cube_mesh = trimesh.creation.box([remove_size, remove_size, z_size * 4])
+        cube_mesh.apply_translation(mesh_copy.centroid - cube_mesh.centroid)
+
+        return trimesh.boolean.difference([mesh_copy, cube_mesh], check_volume=False)
