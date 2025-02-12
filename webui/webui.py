4b9e70670597e729de6b885cdfe6df6e3fcf08b4
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2025-02-12 18:44:07 +0100

    WebUI stats

--- webui/webui.py
+++ webui/webui.py
@@ -1,6 +1,7 @@
 import os
 
 import streamlit as st
+import streamlit.components.v1 as components
 from config import DOCS_DIRECTORY, FAQ_MD, get_mds
 from generator.generator import GeneratorUI
 from templates import Messages
@@ -10,18 +11,29 @@ from toolbox import ToolboxUI
 class WebUI:
     def __init__(self):
         st.set_page_config(page_title="maps4FS", page_icon="🚜", layout="wide")
-        generator_tab, step_by_step_tab, toolbox_tab, knowledge_tab, faq_tab = st.tabs(
-            [
-                "🗺️ Map Generator",
-                "🔢 Step by step",
-                "🧰 Modder Toolbox",
-                "📖 Knowledge base",
-                "📝 FAQ",
-            ]
+        generator_tab, statistics_tab, step_by_step_tab, toolbox_tab, knowledge_tab, faq_tab = (
+            st.tabs(
+                [
+                    "🗺️ Map Generator",
+                    "📊 Statistics",
+                    "🔢 Step by step",
+                    "🧰 Modder Toolbox",
+                    "📖 Knowledge base",
+                    "📝 FAQ",
+                ]
+            )
         )
 
         with generator_tab:
             self.generator = GeneratorUI()
+
+        with statistics_tab:
+            components.iframe(
+                "https://stats.maps4fs.xyz/public/dashboard/"
+                "f8defe6a-09db-4db1-911f-b6b02075d4b2#refresh=60",
+                height=2500,
+                scrolling=False,
+            )
 
         with step_by_step_tab:
             step_by_step_tab_path = os.path.join(DOCS_DIRECTORY, "step_by_step.md")
