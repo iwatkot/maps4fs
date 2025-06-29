2f33c96b4c7fb8198ba85755d8f6ff6d0167d9d4
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2025-06-29 10:58:27 +0200

    Moved DTM Providers to separate repo

--- maps4fs/__init__.py
+++ maps4fs/__init__.py
@@ -1,8 +1,9 @@
 # pylint: disable=missing-module-docstring
+import pydtmdl.providers as dtm
+from pydtmdl import DTMProvider
+
 import maps4fs.generator.component as component
-import maps4fs.generator.dtm as dtm
 import maps4fs.generator.settings as settings
-from maps4fs.generator.dtm.dtm import DTMProvider
 from maps4fs.generator.game import Game
 from maps4fs.generator.map import Map
 from maps4fs.logger import Logger
