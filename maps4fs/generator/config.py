620fd90e884ab21711fcec273a3b156f5958db14
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2025-08-09 08:06:43 +0200

    My Maps BETA3
    * Adjust relative tree shift when using the tree limit.
    
    * Specify data directory.
    
    * Linter updates.
    
    * Specify data directory updates.
    
    * Linter updates.
    
    * Output size.

--- maps4fs/generator/config.py
+++ maps4fs/generator/config.py
@@ -1,15 +1,55 @@
 """This module contains configuration files for the maps4fs generator."""
 
 import os
+import shutil
 import subprocess
 
 from osmnx import settings as ox_settings
+
+from maps4fs.logger import Logger
+
+logger = Logger()
+
+DEFAULT_TEMPLATES_DIR = os.path.join(os.getcwd(), "data")
+MFS_TEMPLATES_DIR = os.getenv("MFS_TEMPLATES_DIRECTORY")
+if MFS_TEMPLATES_DIR is None:
+    logger.info("MFS_TEMPLATES_DIRECTORY is not set. Using default templates directory.")
+    MFS_TEMPLATES_DIR = DEFAULT_TEMPLATES_DIR
+else:
+    logger.info("MFS_TEMPLATES_DIRECTORY is set to: %s", MFS_TEMPLATES_DIR)
+    if not os.path.isdir(MFS_TEMPLATES_DIR):
+        logger.info("Templates directory %s does not exist. Creating it.", MFS_TEMPLATES_DIR)
+        os.makedirs(MFS_TEMPLATES_DIR, exist_ok=True)
+
+    # Check if there any files (directory is empty).
+    if not os.listdir(MFS_TEMPLATES_DIR):
+        logger.info("Templates directory %s is empty. Copying default files.", MFS_TEMPLATES_DIR)
+        for item in os.listdir(DEFAULT_TEMPLATES_DIR):
+            s = os.path.join(DEFAULT_TEMPLATES_DIR, item)
+            d = os.path.join(MFS_TEMPLATES_DIR, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, False, None)
+            else:
+                shutil.copy2(s, d)
+        logger.info("Default files copied to %s.", MFS_TEMPLATES_DIR)
+    else:
+        logger.warning(
+            "Templates directory %s is not empty. Will not copy default files. "
+            "Ensure that the directory contains the necessary template files.",
+            MFS_TEMPLATES_DIR,
+        )
 
 MFS_ROOT_DIR = os.getenv("MFS_ROOT_DIRECTORY", os.path.join(os.getcwd(), "mfsrootdir"))
 MFS_CACHE_DIR = os.path.join(MFS_ROOT_DIR, "cache")
 MFS_DATA_DIR = os.path.join(MFS_ROOT_DIR, "data")
 os.makedirs(MFS_CACHE_DIR, exist_ok=True)
 os.makedirs(MFS_DATA_DIR, exist_ok=True)
+logger.info(
+    "MFS_ROOT_DIR: %s. MFS_CACHE_DIR: %s. MFS_DATA_DIR: %s.",
+    MFS_ROOT_DIR,
+    MFS_CACHE_DIR,
+    MFS_DATA_DIR,
+)
 
 DTM_CACHE_DIR = os.path.join(MFS_CACHE_DIR, "dtm")
 SAT_CACHE_DIR = os.path.join(MFS_CACHE_DIR, "sat")
@@ -50,3 +90,4 @@ def get_package_version(package_name: str) -> str:
 
 
 PACKAGE_VERSION = get_package_version("maps4fs")
+logger.info("maps4fs version: %s", PACKAGE_VERSION)
