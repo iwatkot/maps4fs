7cb192f11d6f48b8b8c3fa3ed896fe75a63c6b97
iwatkot <iwatkot@gmail.com>
2025-10-22 13:33:52 +0200

    License plate prefix default value.

--- maps4fs/generator/settings.py
+++ maps4fs/generator/settings.py
@@ -236,6 +236,7 @@ class I3DSettings(SettingsModel):
             existing points.
         add_reversed_splines (bool): if True, reversed splines will be added to the map.
         field_splines (bool): if True, splines will be added to the fields.
+        license_plate_prefix (str): prefix for the license plates.
     """
 
     add_trees: bool = True
@@ -247,7 +248,7 @@ class I3DSettings(SettingsModel):
     add_reversed_splines: bool = False
     field_splines: bool = False
 
-    license_plate_prefix: str = "ABC"
+    license_plate_prefix: str = "M4S"
 
 
 class TextureSettings(SettingsModel):
