2bbc2b6eb8fe2e816281d73ea5913be3b0c8c250
iwatkot <iwatkot@gmail.com>
2025-11-11 12:01:11 +0100

    Increase CV2 pixel limit.

--- maps4fs/__init__.py
+++ maps4fs/__init__.py
@@ -1,4 +1,14 @@
-# pylint: disable=missing-module-docstring
+# pylint: disable=missing-module-docstring, wrong-import-position
+import cv2
+
+# Increase OpenCV image size limit for large map generation
+# Default is 1024 * 1024 * 1024 = ~1 billion pixels (32,768 x 32,768 images)
+# Setting to 4x larger: 4096 * 1024 * 1024 = ~4 billion pixels (65,536 x 65,536 images)
+
+try:
+    cv2.CV_IO_MAX_IMAGE_PIXELS = 4096 * 1024 * 1024  # type: ignore
+except Exception as e:
+    print(f"Warning: Could not set CV_IO_MAX_IMAGE_PIXELS: {e}")
 import pydtmdl.providers as dtm
 from pydtmdl import DTMProvider
 
